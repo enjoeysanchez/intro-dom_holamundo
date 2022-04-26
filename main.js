ad1fc88caba6c2061cdc2ce0c99805bb1ac0fc14
JoelSN82 <[joelsn82@gmail.com]>
2022-04-25 22:30:48 -0300

    updates

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // alert("Hola Mundo | Intro a DOM desde main.js")
 
+// La const "todos" para que se vayan agregando items al array, y además le pasamos la instruccion de localStorage para que mantenga los datos que se agreguen
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
 // Se crea la const render para ser llamada dentro de ella misma y luego mas adelante en el codigo
 const render = () => {
     const todoList = document.getElementById("todo-list");
@@ -11,16 +14,15 @@ const render = () => {
         elemento.addEventListener("click", () => {
         elemento.parentNode.removeChild(elemento)
         todos.splice(i, 1)
+        actualizarTodos(todos)
         render();
         })            
     })      
 }
 
-// Una const en blanco para que se vayan agregando items al array
-const todos = []
-
 // MAIN INPUT | Cumple la funcion de poder agregar items a nuestra lista
 window.onload = () => {
+    render();
     const form = document.getElementById("todo-form")
     form.onsubmit = (clearField) => {
         clearField.preventDefault();  
@@ -28,12 +30,17 @@ window.onload = () => {
         const todo = document.getElementById("todo");
         const todoText = todo.value;        
         todo.value = "";
-        todos.push(todoText);     
+        todos.push(todoText);    
+        actualizarTodos(todos)
         render (); // Se llama a render
         
     }
 }
 
+const actualizarTodos = (todos) => {
+    const todoStrings = JSON.stringify(todos)
+    localStorage.setItem("todos", todoStrings)
+}
 
 
 
